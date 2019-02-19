e90ef5a3ab1a3bd50dab0a3c5867a9d8bceb112c
Jørgen Sneisen <221950@usn.no>
2019-02-19 12:26:25 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace USBTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.txtTemp = new System.Windows.Forms.TextBox();
+            this.txtPot = new System.Windows.Forms.TextBox();
+            this.txtAlarm = new System.Windows.Forms.TextBox();
+            this.Temp = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // txtTemp
+            // 
+            this.txtTemp.Location = new System.Drawing.Point(222, 78);
+            this.txtTemp.Name = "txtTemp";
+            this.txtTemp.Size = new System.Drawing.Size(100, 20);
+            this.txtTemp.TabIndex = 0;
+            // 
+            // txtPot
+            // 
+            this.txtPot.Location = new System.Drawing.Point(222, 131);
+            this.txtPot.Name = "txtPot";
+            this.txtPot.Size = new System.Drawing.Size(100, 20);
+            this.txtPot.TabIndex = 1;
+            // 
+            // txtAlarm
+            // 
+            this.txtAlarm.Location = new System.Drawing.Point(222, 190);
+            this.txtAlarm.Name = "txtAlarm";
+            this.txtAlarm.Size = new System.Drawing.Size(100, 20);
+            this.txtAlarm.TabIndex = 2;
+            // 
+            // Temp
+            // 
+            this.Temp.AutoSize = true;
+            this.Temp.Location = new System.Drawing.Point(225, 62);
+            this.Temp.Name = "Temp";
+            this.Temp.Size = new System.Drawing.Size(34, 13);
+            this.Temp.TabIndex = 3;
+            this.Temp.Text = "Temp";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(222, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Pot";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(225, 171);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Alarm";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(518, 77);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(645, 75);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Temp);
+            this.Controls.Add(this.txtAlarm);
+            this.Controls.Add(this.txtPot);
+            this.Controls.Add(this.txtTemp);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox txtTemp;
+        private System.Windows.Forms.TextBox txtPot;
+        private System.Windows.Forms.TextBox txtAlarm;
+        private System.Windows.Forms.Label Temp;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace USBTest
+{
+    public partial class Form1 : Form
+    {
+        SerialPort port;
+        string[] values;
+        public Form1()
+        {
+            
+            InitializeComponent();
+            loadPorts();
+            this.FormClosed += new FormClosedEventHandler(Form1_FormClosed);
+            
+        }
+        
+        void loadPorts()
+        {
+            string[] portNames = SerialPort.GetPortNames();
+            foreach (string port in portNames)
+            {
+                comboBox1.Items.Add(port);
+            }
+            comboBox1.SelectedIndex = 0;
+        }
+        void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if(port != null && port.IsOpen)
+            {
+                port.Close();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ask();
+        }
+        void ask()
+        {
+            string message;
+            port.Write("1");
+            message = port.ReadLine();
+            values = message.Split(';');
+            txtTemp.Text = $"{values[0]}°C";
+            txtPot.Text = values[1];
+            txtAlarm.Text = values[2];
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (port == null)
+            {
+                port = new SerialPort(comboBox1.Text, 9600);//BoardCom
+                port.Open();
+                timer1.Start();
+            }
+        }
+    }
+}
